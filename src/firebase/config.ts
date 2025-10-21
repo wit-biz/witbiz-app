7c69795f580a1a020385c37121acc660b04c3ce8
Wit Biz <witbiz.mx@gmail.com>
2025-10-21 01:39:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "wit-biz-20840214-423fc",
+  "appId": "1:167489268583:web:627986979f10320573ddc2",
+  "apiKey": "AIzaSyDsz1VH_NVh-ZpSvdm7m1YQkrWeEVAXSQ8",
+  "authDomain": "wit-biz-20840214-423fc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "167489268583"
+};
