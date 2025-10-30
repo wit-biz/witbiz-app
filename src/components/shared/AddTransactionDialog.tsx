86e76f593be84b6d2037b5b63c9cbe0a9fac4af9
Wit Biz <witbiz.mx@gmail.com>
2025-10-30 23:59:12 +0000

    Try fixing this error: `Runtime Error: Error: useMemo is not defined. Er

--- src/components/shared/AddTransactionDialog.tsx
+++ src/components/shared/AddTransactionDialog.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
