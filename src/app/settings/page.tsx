0d31c6c0e0382df3279a0ccd24247c729bfd9404
Wit Biz <witbiz.mx@gmail.com>
2025-10-31 01:24:22 +0000

    en vez de seleccione uun rango, que sea igual que auditoria, que diga ca

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -209,7 +209,7 @@ export default function SettingsPage() {
                         <PopoverTrigger asChild>
                             <Button id="date" variant={"outline"} className={cn("w-full md:w-auto justify-start text-left font-normal", !date && "text-muted-foreground")}>
                             <CalendarIcon className="mr-2 h-4 w-4" />
-                            {date?.from ? (date.to ? (<>{format(date.from, "LLL dd, y")} - {format(date.to, "LLL dd, y")}</>) : (format(date.from, "LLL dd, y"))) : (<span>Seleccione un rango</span>)}
+                            {date?.from ? (date.to ? (<>{format(date.from, "LLL dd, y")} - {format(date.to, "LLL dd, y")}</>) : (format(date.from, "LLL dd, y"))) : (<span>Calendario</span>)}
                             </Button>
                         </PopoverTrigger>
                         <PopoverContent className="w-auto p-0" align="start">
