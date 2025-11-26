c4f53d48e925dd8dfc140bcc0b08f8464856ff72
Wit Biz <witbiz.mx@gmail.com>
2025-11-26 23:00:05 +0000

    añadelo

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -29,6 +29,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 
 // --- Mock Data ---
