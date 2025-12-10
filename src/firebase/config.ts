361baa274a6eacbabd07b37c1da35fc18d8c612c
Wit Biz <witbiz.mx@gmail.com>
2025-12-10 04:53:29 +0000

    gs://wit-biz-07943714-b10c8.firebasestorage.app

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
   "projectId": "wit-biz-07943714-b10c8",
   "appId": "1:383875580444:web:4a30d780d90ad505cd55d9",
   "apiKey": "AIzaSyDH54EjUgsjGQ4bfL62fLtomFSER4_W9WI",
   "authDomain": "wit-biz-07943714-b10c8.firebaseapp.com",
+  "storageBucket": "wit-biz-07943714-b10c8.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "383875580444"
 };
